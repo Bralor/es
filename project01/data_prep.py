444e2c436cee2aae172034af5cc818afa5369a3d
Matous <bralor92@email.cz>
2021-12-23 15:11:45 +0100

    data_prep.py: json added, variables added

--- project01/data_prep.py
+++ project01/data_prep.py
@@ -1,4 +1,10 @@
+import json
+
 from elasticsearch import Elasticsearch
+
+# filenames
+index_name: str = "gastronomical"
+doc_type: str = "recipes"
 
 # instantiate a new client instance
 client = Elasticsearch("http://localhost:9200")
